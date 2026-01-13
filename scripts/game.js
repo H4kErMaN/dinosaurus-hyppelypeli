3f780d61f3f80e1fac63f636eb73bb23d3cd3789
H4kErMaN <132083093+H4kErMaN@users.noreply.github.com>
2026-01-13 20:03:24 +0200

    Add game canvas and dino drawing functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,30 @@
 // Game functions are build here and used from main.js
+
+let canvas;
+let ctx;
+
+export function createGameCanvas() {
+    canvas = document.createElement("canvas");
+    canvas.id = "gameCanvas";
+    canvas.width = 800;
+    canvas.height = 400;
+    canvas.classList.add("gameCanvas");
+
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+
+    return { canvas, ctx };
+}
+
+export function drawDino() {
+    const dino = new Image();
+    dino.src = "./Dino-.png";
+
+    dino.onload = function() {
+        const x = canvas.width - dino.width - 50;
+        const y = canvas.height - dino.height - 50;
+        ctx.drawImage(dino, x, y);
+    };
+
+    return dino;
+}
